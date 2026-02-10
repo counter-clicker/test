e6851c2364881c2580b1fa0d01c0ebc77ffde710
counter-clicker <nigerhaterhere@gmail.com>
2026-02-10 10:31:21 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+const button = document.getElementById('cta');
+const message = document.getElementById('message');
+
+button.addEventListener('click', () => {
+  message.textContent = 'Top! Je website werkt en is klaar voor jouw volgende stap.';
+});
